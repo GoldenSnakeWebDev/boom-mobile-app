4e54fdf3fbaeb888547a4e7a0c128308bbe5f2c6
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-09-27 19:58:06 +0300

    Home Screen

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:boom_mobile/utils/constants.dart';
 import 'package:boom_mobile/utils/size_config.dart';
 import 'package:boom_mobile/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
@@ -11,37 +12,56 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: const CustomAppBar(),
       body: SafeArea(
-        child: Column(
-          children: [
-            SizedBox(
-              height: getProportionateScreenHeight(100),
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: const EdgeInsets.only(right: 10),
-                    child: Column(
-                      children: [
-                        Container(
-                          width: getProportionateScreenWidth(56),
-                          height: getProportionateScreenHeight(56),
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                                image: AssetImage("assets/image/!sam.jpeg"),
-                                fit: BoxFit.cover),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: getProportionateScreenHeight(100),
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: talesDetails.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 30),
+                      child: Column(
+                        children: [
+                          Container(
+                            width: getProportionateScreenWidth(56),
+                            height: getProportionateScreenHeight(56),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12.0),
+                              image: DecorationImage(
+                                  image: AssetImage(
+                                      "assets/images/${talesDetails[index]["img"]}.jpeg"),
+                                  fit: BoxFit.cover),
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          height: getProportionateScreenHeight(10),
-                        ),
-                        const Text("!Sam")
-                      ],
-                    ),
-                  );
-                },
+                          SizedBox(
+                            height: getProportionateScreenHeight(10),
+                          ),
+                          Text(
+                            talesDetails[index]["title"].toString(),
+                            style: TextStyle(
+                              fontSize: getProportionateScreenHeight(12),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            )
-          ],
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: ((context, index) {
+                    return Container();
+                  }),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,1 +1,22 @@
-
+List<Map<String, String>> talesDetails = [
+  {
+    "img": "!sam",
+    "title": "!Sam",
+  },
+  {
+    "img": "!rottie",
+    "title": "!Rottie",
+  },
+  {
+    "img": "!flexcon",
+    "title": "!Flexcon",
+  },
+  {
+    "img": "!muto",
+    "title": "!Muto",
+  },
+  {
+    "img": "!boom",
+    "title": "!Gelato",
+  },
+];
