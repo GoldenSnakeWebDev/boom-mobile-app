36f6eee277afefa42fe46e58d7b8679b8129e92b
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-09-28 15:42:41 +0300

    Brand Details

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -20,3 +20,41 @@ List<Map<String, String>> talesDetails = [
     "title": "!Gelato",
   },
 ];
+
+List<Map<String, String>> brandDetails = [
+  {
+    "img": "gucci.jpeg",
+    "title": "Gucci",
+    "likes": "1.2K",
+    "reboom": "40",
+    "comments": "20",
+  },
+  {
+    "img": "versace.jpeg",
+    "title": "Versace",
+    "likes": "150",
+    "reboom": "10",
+    "comments": "5",
+  },
+  {
+    "img": "offwhite.jpeg",
+    "title": "Offwhite",
+    "likes": "10.5k",
+    "reboom": "36",
+    "comments": "15",
+  },
+  {
+    "img": "jordans.jpeg",
+    "title": "Jordans",
+    "likes": "1.2K",
+    "reboom": "69",
+    "comments": "36",
+  },
+  {
+    "img": "zara.png",
+    "title": "Zara",
+    "likes": "1574",
+    "reboom": "87",
+    "comments": "70",
+  },
+];
