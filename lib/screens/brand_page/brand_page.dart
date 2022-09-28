889a5629c4e13f3959311ffc65d45b7ef193cc44
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-09-28 19:58:21 +0300

    Single Brand NFT Page

--- lib/screens/brand_page/brand_page.dart
+++ lib/screens/brand_page/brand_page.dart
@@ -1,7 +1,10 @@
+import 'package:boom_mobile/screens/brand_page/single_nft_page.dart';
 import 'package:boom_mobile/utils/colors.dart';
+import 'package:boom_mobile/utils/constants.dart';
 import 'package:boom_mobile/utils/size_config.dart';
 import 'package:boom_mobile/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class BrandPage extends StatelessWidget {
   final String title;
@@ -19,7 +22,7 @@ class BrandPage extends StatelessWidget {
               decoration: const BoxDecoration(color: Colors.black),
               alignment: Alignment.center,
               child: Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                padding: const EdgeInsets.only(top: 6.0, bottom: 6.0),
                 child: Text(
                   "$title: Home",
                   style: TextStyle(
@@ -32,45 +35,57 @@ class BrandPage extends StatelessWidget {
             ),
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(8.0),
+                padding: const EdgeInsets.fromLTRB(16, 30, 16, 0),
                 child: GridView.builder(
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 2,
-                    childAspectRatio: 0.66,
+                    childAspectRatio: 0.63,
                     crossAxisSpacing: 30,
                   ),
+                  itemCount: brandNFTs.length,
                   itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.only(bottom: 10),
-                      child: Column(
-                        children: [
-                          Container(
-                            height: getProportionateScreenHeight(200),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(12),
-                                image: const DecorationImage(
-                                  image: NetworkImage(
-                                    "https://s3-alpha-sig.figma.com/img/a0ed/d9fd/9d276f722b9431bca5bab749058cda8b?Expires=1665360000&Signature=IK71Bp3tI4DJJyi7OXS-ryXKIRcAFvXk9QxlbBsYp4YjijuFdrRIe604DH5Mb9NiCeh0iJ44ZjfmV0Q-KMqg~XBfAw80mb6riV4BDavlr5lnhFsLM6ejeOhN60BnDzqquU57fmrY2KdALCb7d4GuH0YWOgJlR8-xZnMjQLwvEEAxnFx4bXiKcB5viYBRYL~TA8qQ8q0NzqjRRKTSp3xZBQySMKaCvTe1kv95dSzw9unSm~BYG02z92~4aImySQMIR9FfQ8zVAMOhLPiaTwMy1uK~D-K-HsXv-kVtld2vZUOWF2fDFELCHVO-GWyHhU3YtldxjIj93eFz6XxFmrF~EA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+                    return GestureDetector(
+                      onTap: () {
+                        Get.to(
+                          () => SingleNFTPage(
+                            title: brandNFTs[index]["title"].toString(),
+                            brandName: title,
+                          ),
+                        );
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.only(bottom: 10),
+                        child: Column(
+                          children: [
+                            Container(
+                              height: getProportionateScreenHeight(200),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(12),
+                                  image: DecorationImage(
+                                    image: NetworkImage(
+                                      brandNFTs[index]["img"].toString(),
+                                    ),
+                                    fit: BoxFit.cover,
                                   ),
-                                ),
-                                border: Border.all(color: Colors.black)),
-                          ),
-                          SizedBox(
-                            height: getProportionateScreenHeight(7),
-                          ),
-                          Container(
-                            decoration: BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(6)),
-                            child: const Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Text(
-                                "NFT",
-                                style: TextStyle(color: Colors.white),
-                              ),
+                                  border: Border.all(color: Colors.black)),
                             ),
-                          )
-                        ],
+                            SizedBox(
+                              height: getProportionateScreenHeight(7),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(6)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  brandNFTs[index]["title"].toString(),
+                                  style: const TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     );
                   },
