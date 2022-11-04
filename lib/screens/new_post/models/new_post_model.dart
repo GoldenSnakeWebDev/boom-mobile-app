f1ae99902d585ff83fb779b11c84a86d12244535
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-11-04 11:58:08 +0300

    Create New boom

---
+++ lib/screens/new_post/models/new_post_model.dart
@@ -0,0 +1,32 @@
+// "boom_type" : "image",
+// "network": "6362b237b11f989c35773a4b",
+// "description":"SOME description",
+// "image_url": "http://localhost:4000/NFTS/NFTS-a46c2e4e-5b67-11ed-9234-0123456789ab-1667473757993.png",
+// "title":"The First Boom",
+// "quantity": "10",
+// "tags": "some,next,booms,andNewWeGetIt",
+// "fixed_price": "0",
+// "price":"1.02"
+
+class NewPostModel {
+  final String boomType;
+  final String network;
+  final String description;
+  final String title;
+  final String imageUrl;
+  final String quantity;
+  final String tags;
+  final String fixedPrice;
+  final String price;
+
+  NewPostModel(
+      {required this.boomType,
+      required this.network,
+      required this.description,
+      required this.title,
+      required this.imageUrl,
+      required this.quantity,
+      required this.tags,
+      required this.fixedPrice,
+      required this.price});
+}
---
+++ lib/screens/new_post/services/upload_boom.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+
+import 'package:boom_mobile/screens/new_post/models/new_post_model.dart';
+import 'package:boom_mobile/utils/url_container.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:http/http.dart' as http;
+
+class UploadService {
+  final box = GetStorage();
+  uploadPost(NewPostModel boom) async {
+    String token = box.read("token");
+    final Map<String, dynamic> body = {
+      "boom_type": boom.boomType,
+      "network": boom.network,
+      "description": boom.description,
+      "image_url": boom.imageUrl,
+      "title": boom.title,
+      "quantity": boom.quantity,
+      "tags": boom.tags,
+      "fixed_price": boom.fixedPrice,
+      "price": boom.price
+    };
+    final res = http.post(Uri.parse("${baseURL}booms"),
+        headers: {"Authorization": token}, body: jsonEncode(body));
+
+    return res;
+  }
+}
