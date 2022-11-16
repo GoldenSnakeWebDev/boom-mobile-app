9107a690664f02162bb42e5bbbf691b653a092eb
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-11-16 20:01:44 +0300

    Connect Wallet

--- lib/screens/new_post/controllers/new_post_controller.dart
+++ lib/screens/new_post/controllers/new_post_controller.dart
@@ -10,17 +10,54 @@ import 'package:boom_mobile/screens/new_post/models/new_post_model.dart';
 import 'package:boom_mobile/screens/new_post/services/upload_boom.dart';
 import 'package:boom_mobile/screens/profile_screen/controllers/edit_profile_controler.dart';
 import 'package:boom_mobile/widgets/custom_snackbar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:wallet_connect/models/jsonrpc/json_rpc_request.dart';
-import 'package:wallet_connect/wallet_connect.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:walletconnect_dart/walletconnect_dart.dart';
+import 'package:web3dart/crypto.dart';
+
 import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart' as http;
 
 enum POST_TYPE { image, video, text }
+
+class WalletConnectEthereumCredentials extends CustomTransactionSender {
+  final EthereumWalletConnectProvider provider;
+  WalletConnectEthereumCredentials({required this.provider});
+
+  @override
+  Future<EthereumAddress> extractAddress() {
+    // TODO: implement extractAddress
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> sendTransaction(Transaction transaction) async {
+    log("Attempting to send TX");
+    final hash = await provider.sendTransaction(
+      from: transaction.from!.hex,
+      to: transaction.to?.hex,
+      data: transaction.data,
+      gas: transaction.maxGas,
+      gasPrice: transaction.gasPrice?.getInWei,
+      value: transaction.value?.getInWei,
+      nonce: transaction.nonce,
+    );
+
+    return hash;
+  }
+
+  @override
+  Future<MsgSignature> signToSignature(Uint8List payload,
+      {int? chainId, bool isEIP1559 = false}) {
+    // TODO: implement signToSignature
+    throw UnimplementedError();
+  }
+}
 
 class NewPostController extends GetxController {
   final ImagePicker _picker = ImagePicker();
@@ -40,11 +77,15 @@ class NewPostController extends GetxController {
   Network? selectedNetworkModel;
   List<Network> networks = [];
   final igController = Get.find<InstagramWebController>();
-  late WCClient wcClient;
+  // late WCClient wcClient;
   late InAppWebViewController webViewController;
   late String walletAddress, privateKey;
   bool isWalletConnected = false;
-  WCSessionStore? sessionStore;
+  EthereumWalletConnectProvider? provider;
+  Web3Client client = Web3Client(
+      'https://goerli.infura.io/v3/3f83d628804547b89b1f7a84ea02cea9',
+      http.Client());
+  // WCSessionStore? sessionStore;
 
   String rpc =
       'wc:00e46b69-d0cc-4b3e-b6a2-cee442f97188@1?bridge=https%3A%2F%2Fbridge.walletconnect.org&key=91303dedf64285cbbaf9120f6e9d160a5c8aa3deb67017a3874cd272323f48ae';
@@ -66,45 +107,49 @@ class NewPostController extends GetxController {
     log("Ig Post ${igController.selectedIgMedia?.id}");
   }
 
-  connectWallet() {
-    wcClient = WCClient(
-      onSessionRequest: (number, peerData) {
-        log("Session Request");
-      },
-      onFailure: (message) {
-        log("Failure");
-      },
-      onDisconnect: (code, reason) {
-        log("Disconnect");
-      },
-      onEthSign: (id, message) {
-        log("Eth Sign");
-      },
-      onEthSignTransaction: (id, transaction) {
-        log("Eth Sign Transaction");
-      },
-      onConnect: () {
-        log("Connected");
-        isWalletConnected = true;
-        update();
-      },
-      onCustomRequest: (number, peerData) {
-        log("Session Update");
-      },
-      onWalletSwitchNetwork: (id, chainId) {
-        log("Wallet Switch Network");
-      },
+  connectWallet() async {
+    final connector = WalletConnect(
+      bridge: "https://bridge.walletconnect.org",
+      clientMeta: const PeerMeta(
+        name: "Boom",
+        description: "Boom",
+        icons: ["https://boomapp.io/assets/images/logo.png"],
+        url: "https://boomapp.io",
+      ),
     );
-    final rpcReq = JsonRpcRequest.fromJson({
-      "jsonrpc": "2.0",
-      "id": 1666695490479571,
-      "method": "eth_accounts",
-      "params": [
-        {"chainId": "0x1"}
-      ],
+    connector.on('connect', (session) {
+      log("Connect $session");
     });
 
-    log(rpcReq.toJson().toString());
+    if (!connector.connected) {
+      final session = await connector.createSession(
+        chainId: 5,
+        onDisplayUri: (uri) async {
+          log(uri.toString());
+          await launchUrl(Uri.parse(uri));
+        },
+      );
+      connector.connect();
+      provider = EthereumWalletConnectProvider(connector, chainId: 5);
+      final sender = EthereumAddress.fromHex(session.accounts.first);
+
+      final transaction = Transaction(
+        to: EthereumAddress.fromHex(
+            "0x6582436697029990185E7073f386769979aDbc14"),
+        from: sender,
+        gasPrice: EtherAmount.inWei(BigInt.one),
+        maxGas: 100000,
+        value: EtherAmount.fromUnitAndValue(EtherUnit.ether, 1),
+      );
+      final credentials = WalletConnectEthereumCredentials(provider: provider!);
+      log("Client is sending TX");
+      final txBytes = await client.sendTransaction(credentials, transaction);
+      log("Here we are");
+      connector.connect();
+      log(txBytes);
+
+      return txBytes;
+    }
   }
 
   changeChain(String value) {
