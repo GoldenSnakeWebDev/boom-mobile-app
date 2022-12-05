ed27c9cb5b45bd36767d1979e094f799e182c139
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2022-12-05 22:59:04 +0300

    Reactions Fix

--- lib/screens/new_post/ui/instagram_posts.dart
+++ lib/screens/new_post/ui/instagram_posts.dart
@@ -10,8 +10,136 @@ import 'package:get/get.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:video_player/video_player.dart';
 
-class InstagramPosts extends StatelessWidget {
+class InstagramPosts extends StatefulWidget {
   const InstagramPosts({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<InstagramPosts> createState() => _InstagramPostsState();
+}
+
+class _InstagramPostsState extends State<InstagramPosts>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _currentTabIndex = 0;
+  List<Tab> tabs = [
+    const Tab(text: "Photos"),
+    const Tab(text: "Videos"),
+  ];
+  List<Widget> tabViews = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+    tabViews = [
+      const IGPhotosPage(),
+      const IGVideosPage(),
+    ];
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _tabController.addListener(() {
+      log("Anything");
+      setState(() {
+        _currentTabIndex = _tabController.index;
+      });
+    });
+    // return GetBuilder<InstagramWebController>(
+    //     init: InstagramWebController(),
+    //     builder: (controller) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: TextButton(
+          child: const Text("Cancel"),
+          onPressed: () {
+            Get.off(() => const CreateNewPost());
+          },
+        ),
+        leadingWidth: getProportionateScreenWidth(70),
+        title: Text(
+          "Title",
+          // '${controller.medias.isNotEmpty ? controller.medias[0].username : ""} Instagram ${_currentTabIndex == 0 ? "Photos" : "Videos"}',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w700,
+            fontSize: getProportionateScreenHeight(12),
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          TextButton(
+              onPressed: () {
+                // controller.proceedWithUpload(controller.selectedIgMedia);
+              },
+              child: const Text(
+                "Next",
+                style: TextStyle(color: Colors.blue),
+              ))
+        ],
+      ),
+      body: DefaultTabController(
+        length: tabViews.length,
+        child: Stack(
+          children: [
+            Positioned(
+              top: 0,
+              child: TabBar(
+                tabs: [
+                  tabs[0],
+                  tabs[1],
+                ],
+                unselectedLabelColor: Colors.black,
+                labelColor: kPrimaryColor,
+                labelStyle: TextStyle(
+                  fontSize: getProportionateScreenHeight(20),
+                  fontWeight: FontWeight.w700,
+                  color: kPrimaryColor,
+                ),
+                unselectedLabelStyle: TextStyle(
+                  fontSize: getProportionateScreenHeight(20),
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black,
+                ),
+                indicatorColor: Colors.transparent,
+                isScrollable: true,
+              ),
+            ),
+            Positioned(
+              top: getProportionateScreenHeight(40),
+              child: SizedBox(
+                height: SizeConfig.screenHeight * 0.9,
+                width: SizeConfig.screenWidth,
+                child: TabBarView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  controller: _tabController,
+                  children: tabs.map((Tab e) {
+                    return tabViews[_currentTabIndex];
+                  }).toList(),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+    // });
+    // const IGPhotosPage();
+  }
+}
+
+class IGPhotosPage extends StatelessWidget {
+  const IGPhotosPage({
     Key? key,
   }) : super(key: key);
 
@@ -21,40 +149,10 @@ class InstagramPosts extends StatelessWidget {
       init: InstagramWebController(),
       builder: (controller) {
         return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            leading: TextButton(
-              child: const Text("Cancel"),
-              onPressed: () {
-                Get.off(() => const CreateNewPost());
-              },
-            ),
-            leadingWidth: getProportionateScreenWidth(70),
-            title: Text(
-              '${controller.medias.isNotEmpty ? controller.medias[0].username : ""} Instagram Posts',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w700,
-                fontSize: getProportionateScreenHeight(12),
-              ),
-            ),
-            centerTitle: true,
-            actions: [
-              TextButton(
-                  onPressed: () {
-                    controller.proceedWithUpload(controller.selectedIgMedia);
-                  },
-                  child: const Text(
-                    "Next",
-                    style: TextStyle(color: Colors.blue),
-                  ))
-            ],
-          ),
           body: Padding(
             padding: const EdgeInsets.all(8.0),
             child: GridView.builder(
-              itemCount: controller.medias.length,
+              itemCount: controller.igPhotos.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 childAspectRatio: 0.9,
                 crossAxisCount: 3,
@@ -62,29 +160,116 @@ class InstagramPosts extends StatelessWidget {
                 crossAxisSpacing: 10,
               ),
               itemBuilder: (context, index) {
-                return controller.medias[index].media_type == "VIDEO"
+                return GestureDetector(
+                  onTap: () {
+                    controller.selectImage(controller.igPhotos[index]);
+                  },
+                  onLongPress: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) => PreviewIgPostDialog(
+                        image: controller.igPhotos[index].media_url,
+                      ),
+                    );
+                  },
+                  child: Container(
+                    height: getProportionateScreenHeight(50),
+                    width: getProportionateScreenWidth(50),
+                    decoration: BoxDecoration(
+                      border: controller.selectedIgMedia != null &&
+                              controller.selectedIgMedia ==
+                                  controller.igPhotos[index]
+                          ? Border.all(
+                              color: kPrimaryColor,
+                              width: 2,
+                            )
+                          : null,
+                    ),
+                    child: Stack(
+                      children: [
+                        CachedNetworkImage(
+                          height: getProportionateScreenHeight(400),
+                          width: getProportionateScreenWidth(250),
+                          imageUrl: controller.igPhotos[index].media_url,
+                          fit: BoxFit.cover,
+                        ),
+                        controller.selectedIgMedia == null ||
+                                controller.selectedIgMedia !=
+                                    controller.igPhotos[index]
+                            ? const SizedBox()
+                            : Positioned(
+                                bottom: 5,
+                                right: 5,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.white.withOpacity(0.6),
+                                      shape: BoxShape.circle),
+                                  child: const Icon(
+                                    MdiIcons.check,
+                                    color: kPrimaryColor,
+                                    size: 16,
+                                  ),
+                                ),
+                              )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class IGVideosPage extends StatelessWidget {
+  const IGVideosPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    log("IGVideosPage");
+    return GetBuilder<InstagramWebController>(
+      init: InstagramWebController(),
+      builder: (controller) {
+        return Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GridView.builder(
+              itemCount: controller.igVideos.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                childAspectRatio: 0.9,
+                crossAxisCount: 3,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+              ),
+              itemBuilder: (context, index) {
+                return controller.igVideos[index].media_type == "VIDEO"
                     ? GestureDetector(
                         onTap: () {
                           log("Playing video");
                           controller.videoControllers[0]
-                                  [controller.medias[index].id]!
+                                  [controller.igVideos[index].id]!
                               .play();
                         },
                         child: VideoPlayer(
                           VideoPlayerController.network(
-                              controller.medias[index].media_url),
+                              controller.igVideos[index].media_url),
                         ),
                       )
                     : GestureDetector(
                         onTap: () {
-                          controller.selectImage(controller.medias[index]);
+                          controller.selectImage(controller.igVideos[index]);
                         },
                         onLongPress: () {
                           showDialog(
                             context: context,
                             builder: (BuildContext context) =>
                                 PreviewIgPostDialog(
-                              image: controller.medias[index].media_url,
+                              image: controller.igVideos[index].media_url,
                             ),
                           );
                         },
@@ -94,7 +279,7 @@ class InstagramPosts extends StatelessWidget {
                           decoration: BoxDecoration(
                             border: controller.selectedIgMedia != null &&
                                     controller.selectedIgMedia ==
-                                        controller.medias[index]
+                                        controller.igVideos[index]
                                 ? Border.all(
                                     color: kPrimaryColor,
                                     width: 2,
@@ -106,12 +291,12 @@ class InstagramPosts extends StatelessWidget {
                               CachedNetworkImage(
                                 height: getProportionateScreenHeight(400),
                                 width: getProportionateScreenWidth(250),
-                                imageUrl: controller.medias[index].media_url,
+                                imageUrl: controller.igVideos[index].media_url,
                                 fit: BoxFit.cover,
                               ),
                               controller.selectedIgMedia == null ||
                                       controller.selectedIgMedia !=
-                                          controller.medias[index]
+                                          controller.igVideos[index]
                                   ? const SizedBox()
                                   : Positioned(
                                       bottom: 5,
