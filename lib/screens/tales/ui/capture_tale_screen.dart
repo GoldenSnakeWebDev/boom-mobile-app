d29e409c02cf8407f72ee5823a760804f483e2a8
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2023-02-14 01:47:20 +0300

    Tales and Epics

--- lib/screens/tales/ui/capture_tale_screen.dart
+++ lib/screens/tales/ui/capture_tale_screen.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
-import 'package:boom_mobile/di/app_bindings.dart';
 import 'package:boom_mobile/screens/tales/controllers/camera_controller.dart';
-import 'package:boom_mobile/screens/tales/ui/edit_tale_image.dart';
-import 'package:boom_mobile/utils/size_config.dart';
-import 'package:camera/camera.dart';
+import 'package:camerawesome/camerawesome_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -28,28 +23,41 @@ class _CaptureTaleScreenState extends State<CaptureTaleScreen> {
   }
 
   @override
+  void dispose() {
+    controller.onClose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
         child: Stack(
           children: [
-            controller.cameraController.value.isInitialized
-                ? controller.cameraController.buildPreview()
-                : Container(),
-            Positioned(
-              top: 5,
-              left: SizeConfig.screenWidth * 0.4,
-              child: GestureDetector(
-                onTap: () {
-                  controller.cameraController.setFlashMode(FlashMode.off);
-                },
-                child: const Icon(
-                  MdiIcons.flashOff,
-                  color: Colors.white,
-                ),
+            // controller.cameraController.value.isInitialized
+
+            //     ? controller.cameraController.buildPreview()
+            //     : Container(),
+            CameraAwesomeBuilder.awesome(
+              saveConfig: SaveConfig.photo(
+                pathBuilder: () => controller.getPath(CaptureMode.photo),
               ),
+              flashMode: FlashMode.none,
+              onMediaTap: (mediaCapture) {},
             ),
+
+            // Positioned(
+            //   top: 5,
+            //   left: SizeConfig.screenWidth * 0.4,
+            //   child: GestureDetector(
+            //     onTap: () {},
+            //     child: const Icon(
+            //       MdiIcons.flashOff,
+            //       color: Colors.white,
+            //     ),
+            //   ),
+            // ),
             Positioned(
               top: 5,
               right: 10,
@@ -63,57 +71,62 @@ class _CaptureTaleScreenState extends State<CaptureTaleScreen> {
                 ),
               ),
             ),
-            const Positioned(
-              bottom: 25,
-              right: 10,
-              child: Icon(
-                MdiIcons.cameraFlip,
-                color: Colors.white,
-              ),
-            ),
-            const Positioned(
-              bottom: 25,
-              left: 10,
-              child: Icon(
-                MdiIcons.imageAreaClose,
-                color: Colors.white,
-              ),
-            ),
+            // const Positioned(
+            //   bottom: 25,
+            //   right: 10,
+            //   child: Icon(
+            //     MdiIcons.cameraFlip,
+            //     color: Colors.white,
+            //   ),
+            // ),
             Positioned(
-              bottom: 60,
-              left: SizeConfig.screenWidth * 0.4,
+              bottom: 55,
+              left: 10,
               child: GestureDetector(
-                onTap: () async {
-                  XFile? capturedImage =
-                      await controller.cameraController.takePicture();
-                  File file = File(capturedImage.path);
-                  Get.to(
-                    () => EditTaleImage(imageFile: file),
-                    binding: AppBindings(),
-                  );
+                onTap: () {
+                  controller.pickPhoto();
                 },
-                child: Container(
-                  width: getProportionateScreenWidth(55),
-                  height: getProportionateScreenHeight(55),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    shape: BoxShape.circle,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Container(
-                      width: getProportionateScreenWidth(45),
-                      height: getProportionateScreenHeight(45),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2),
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  ),
+                child: const Icon(
+                  MdiIcons.imageAreaClose,
+                  color: Colors.white,
                 ),
               ),
-            )
+            ),
+            // Positioned(
+            //   bottom: 60,
+            //   left: SizeConfig.screenWidth * 0.4,
+            //   child: GestureDetector(
+            //     onTap: () async {
+            //       XFile? capturedImage =
+            //           await controller.cameraController.takePicture();
+            //       File file = File(capturedImage.path);
+            //       Get.to(
+            //         () => EditTaleImage(imageFile: file),
+            //         binding: AppBindings(),
+            //       );
+            //     },
+            //     child: Container(
+            //       width: getProportionateScreenWidth(55),
+            //       height: getProportionateScreenHeight(55),
+            //       decoration: const BoxDecoration(
+            //         color: Colors.white,
+            //         shape: BoxShape.circle,
+            //       ),
+            //       child: Padding(
+            //         padding: const EdgeInsets.all(4.0),
+            //         child: Container(
+            //           width: getProportionateScreenWidth(45),
+            //           height: getProportionateScreenHeight(45),
+            //           decoration: BoxDecoration(
+            //             border: Border.all(color: Colors.black, width: 2),
+            //             color: Colors.white,
+            //             shape: BoxShape.circle,
+            //           ),
+            //         ),
+            //       ),
+            //     ),
+            //   ),
+            // )
           ],
         ),
       ),
