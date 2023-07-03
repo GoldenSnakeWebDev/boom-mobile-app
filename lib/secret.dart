77c4ca4added3d1a4edba61365c7c66bdf97db62
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2023-07-03 14:41:43 +0300

    Secret.dart for Workflow

---
+++ lib/secret.dart
@@ -0,0 +1,23 @@
+const String clientID = "";
+const String appSecret = "";
+const String redirectUri = "";
+const String scope = '';
+const String responseType = '';
+const String url = '';
+
+/// Presets your required fields on each call api.
+/// Please refers to https://developers.facebook.com/docs/instagram-basic-display-api/reference .
+List<String> userFields = [];
+List<String> mediaListFields = [];
+List<String> mediaFields = [];
+
+//NFT Storage API Keys
+const String nftStorageApiKey = "";
+
+const String polygonTestApiKey = "";
+
+const String oneSignalAppId = "";
+
+const String firebaseAPIKey = "";
+
+const String firebaseIosAPI = "";
