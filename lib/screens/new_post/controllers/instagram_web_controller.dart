9d63ee2f704846d68ad5a12f41c1aefb3e0423a5
GoldenSnakeWebDev <goldensnake.web.dev@gmail.com>
2023-08-14 23:24:56 +0300

    Meta Developer Credentials Change

--- lib/screens/new_post/controllers/instagram_web_controller.dart
+++ lib/screens/new_post/controllers/instagram_web_controller.dart
@@ -13,7 +13,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_webview_plugin_ios_android/flutter_webview_plugin_ios_android.dart';
-
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
@@ -32,16 +31,29 @@ class InstagramWebController extends GetxController {
   InstaMedia? selectedIgMedia;
   String filePath = '';
   late TabController tabController;
+  double loadingProgress = 0.0;
 
   @override
   void onInit() async {
     super.onInit();
     await urlChanged();
+    await onProgressChanged();
   }
 
   changeTabs() {}
 
+  onProgressChanged() async {
+    flutterWebViewPlugin.onProgressChanged.listen((double progress) {
+      log("Progress $progress");
+      loadingProgress = progress;
+      update();
+    });
+
+    update();
+  }
+
   urlChanged() async {
+    log("We are logging URL Changes vulai");
     onUrlChanged = flutterWebViewPlugin.onUrlChanged.listen((String url) async {
       log("New Url $url");
       if (url.startsWith("https://www.boooooooooom.com/")) {
@@ -67,12 +79,14 @@ class InstagramWebController extends GetxController {
         }
       } else if (url
           .startsWith("https://www.instagram.com/accounts/manage_access/")) {
-        Get.back();
+        // Get.back();
+        log("URL is $url");
       } else {
         log("URL $url");
         log("Not found token and user id");
       }
     });
+    log("Url Changed");
   }
 
   sortMedia(List<InstaMedia> media) async {
